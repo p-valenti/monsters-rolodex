587d1f463c67ad6959cfeca55d9e7a11e451557c
Valentina Popova <solval89@gmail.com>
2022-04-20 23:30:09 +0300

    Added monster's id in card list component.

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -6,7 +6,7 @@ class cardList extends Component {
         const {monsters} = this.props;
         return <div>
             {monsters.map(monster =>(
-                <h1>{monster.name}</h1>
+                <h1 key={monster.id}>{monster.name}</h1>
             ))}
         </div>;
     }

cb24fa68bc2c0f2599b45b8bbc99c5f8c37d6cb1
Valentina Popova <solval89@gmail.com>
2022-04-21 11:36:34 +0300

    Added elements in monster's cards

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -4,11 +4,17 @@ class CardList extends Component {
 
     render() {
         const {monsters} = this.props;
-        return <div>
+        return (
+            <div className='card-list'>
             {monsters.map(monster =>(
-                <h1 key={monster.id}>{monster.name}</h1>
+                <div className='card-container' key={monster.id}>
+                    <img alt={`monster ${monster.name}`} src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}/>
+                    <h2>{monster.name}</h2>
+                    <p>{monster.email}</p>
+                </div>
             ))}
-        </div>;
+        </div>
+        );
     }
 }
 
